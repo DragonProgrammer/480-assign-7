802f1d56fe7e1dbdd1e709426b2a7cb9b94de844
Michael Peterson <Mike_Jon_Peterson@gmail.com>
2019-12-02 21:32:18 -0600

    one function not working, look into Name(Name, NewName)

--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@
 #include <iostream>
 #include <iterator>
 #include <list>
+#include <string>
 #define BLOCK 512
-#define DIRE 4
+#define DIRE 12
 #define OUT 24
+#define OFTEN 6
 using std::array;
 using std::cout;
 using std::endl;
+using std::ifstream;
 using std::list;
+using std::string;
 list<file> directory;
 int FAT[400];
 string SName; // for search name so can cerror
@@ -52,7 +56,15 @@ void OutEntry() {
   }
 }
 
-
+/**
+ * function firstAVA
+ *
+ * use finde first 0 in FAT
+ *
+ * arguments none
+ *
+ * return postion of first 0
+ */
 int firstAVA() {
   int i = 0;
   while (FAT[i] != 0) {
@@ -61,6 +73,15 @@ int firstAVA() {
   return i;
 }
 
+/**
+ * function nextAVA
+ *
+ * use finde second 0 in FAT
+ *
+ * arguments none
+ *
+ * return postion of second 0
+ */
 int nextAVA() {
   int f = firstAVA();
   f++;
@@ -70,6 +91,15 @@ int nextAVA() {
   return f;
 }
 
+/**
+ * function Search
+ *
+ * use look for a file matintch a give name
+ *
+ * arguments name string
+ *
+ * return the file
+ */
 file Search(string N) {
   int spot = 0;
   SName = N;
@@ -81,33 +111,45 @@ file Search(string N) {
   }
   return file();
 }
+/**
+ * function Delete
+ *
+ * use Delete a given file from the directory list
+ *
+ * arguments the file object to be deleted
+ *
+ * return none
+ */
 void Delete(file e) {
   auto spot = directory.begin();
   for (auto &D : directory) {
     if (e.Name == D.Name) {
       directory.erase(spot);
-    //  return;
+      //  return;
       cout << "match";
-   return;
+      return;
     }
     spot++;
   }
 }
 
-
+/**
+ * function RemFat
+ *
+ * use Set to zero all clusters used by file
+ *
+ * arguments the file object
+ *
+ * return none
+ */
 void RemFAT(file e) {
   if (e.Name == " ") {
     cout << endl << "File " << SName << " not found." << endl;
     return;
   }
   int next = e.Start;
- // if (next == -1) {
-   // FAT[e.Start] = 0;
-  //  Delete(e);
-  //  return;
- // }
-  int cur = next;
-  for (int i = 1; i<= e.Clusters; i++) {
+  int cur = next;//postion of next thing to remove
+  for (int i = 1; i <= e.Clusters; i++) {
     if (cur == -1) {
       cout << "off by one";
     }
@@ -117,8 +159,15 @@ void RemFAT(file e) {
   }
   Delete(e);
 }
-
-
+/**
+ * function OutFat
+ *
+ * use Output a Defined first part of FAR
+ *
+ * arguments none
+ *
+ * return none
+ */
 void OutFAT() {
   cout << endl;
   for (int j = 0; j < OUT; j++) {
@@ -131,12 +180,24 @@ void OutFAT() {
   cout << endl;
 }
 
-
+/**
+ * function AddFat
+ *
+ * use add a file with a given name and size to directory, and fill in FAT for it
+ *
+ * arguments 1) The name string
+ * 		2) the size int
+ *
+ * return none
+ */
 void AddFAT(string N, int S) {
   file temp(N, S, firstAVA());
-cout << temp.Start << endl;
-  if(temp.Start==0){ directory.push_front(temp);} //replaces directory block
-else{ directory.push_back(temp);}
+  if (temp.Start == 0) {
+    directory.push_front(temp);
+  } // replaces directory block
+  else {
+    directory.push_back(temp);
+  }
   int F = temp.Start;
   int Sec = nextAVA();
   for (int i = 0; i < temp.Clusters; i++) {
@@ -150,27 +211,54 @@ else{ directory.push_back(temp);}
   }
 }
 
-
-void CopyFAT(string N){
-file e=	Search(N);
-	AddFAT(e.Name, e.Size);
+/**
+ * function CopyFat
+ *
+ * use add a new file to directory and FAT that is the same size as a given file but new name
+ *
+ * arguments N, the string name of the selected fiel
+ * 		R, the string name for the new file
+ *
+ * return none
+ */
+void CopyFAT(string N, string R) {
+  file e = Search(N);
+  AddFAT(R, e.Size);
 }
 
-void AddDB(file d){
-	int a= directory.size()+1;
-	int b= DIRE * d.Clusters; // casting signed an unsigned to ints
-	if (a >b){
-		string N= d.Name;
-		int S = d.Size+BLOCK;
-		RemFAT(Search(N));
-OutFAT();
-		cout << "in here" <<endl;
-		AddFAT(N,S);
+/**
+ * function AddDB
+ *
+ * use See if the Database file needs a new block, and if change the database to use one more cluster
+ *
+ * arguments The database file object
+ *
+ * return none
+ */
+void AddDB(file d) {
+  int a = directory.size() + 1;
+  int b = DIRE * d.Clusters; // casting signed an unsigned to ints
+  if (a > b) {
+    string N = d.Name;
+    int S = d.Size + BLOCK;
+    RemFAT(Search(N));
+    OutFAT();
+    AddFAT(N, S);
 
-		return;
-	}
-	cout << "Not needed" << endl;
+    return;
+  }
 }
+
+/**
+ * function Name
+ *
+ * use Rename a file in directory
+ *
+ * arguments O, the name of the origianl
+ * 		N, The new name
+ *
+ * return none
+ */
 void Name(string O, string N) {
   auto spot = directory.begin();
   for (auto &D : directory) {
@@ -182,6 +270,16 @@ void Name(string O, string N) {
   }
 }
 
+/**
+ * function EditFAT
+ *
+ * use change the size of an existing file
+ *
+ * arguments the original file object
+ * 		the new size int
+ *
+ * return none
+ */
 void EditFAT(file e, int s) {
   string n = e.Name;
   AddFAT("TEMP", s);
@@ -190,33 +288,40 @@ void EditFAT(file e, int s) {
 }
 
 int main() {
-  // OutFAT();
-  // cout << firstAVA();
+  ifstream Infile;
+  Infile.open("testdata");
+  char Case;
+  string Name, NewName;
+  int Size;
   AddFAT(".", 512);
   AddFAT("..", 0);
-  AddFAT("first", 512);
-  AddDB(Search("."));
-  AddFAT("second", 500);
-//  OutFAT();
- // OutEntry();
-//RemFAT(Search("first"));
+  int time = 0;
+  while (Infile >> Case)
+    if (time % OFTEN == 0) {
+      OutFAT();
+      OutEntry();
+    }
+  switch (Case) {
+  case 'N':
+    AddDB(Search("."));
+    Infile >> Name >> Size;
+    AddFAT(Name, Size);
 
-	  AddDB(Search("."));
-  AddFAT("third", 2000);
-  OutFAT();
-//  OutEntry();
-//  AddFAT("Fourth", 1500);
- // AddFAT("five", 5);
-//RemFAT(Search("third"));
-
- // Name("five", "FIVE");
- // OutEntry();
- // EditFAT(Search("Fourth"),2500);
-  //OutFAT();
-
-
-//RemFAT(Search("first"));
-//OutFAT();
-  OutEntry();
-  return 0;
+  case 'C':
+    Infile >> Name >> NewName;
+    AddDB(Search("."));
+    CopyFAT(Name, NewName);
+  case 'D':
+    Infile >> Name;
+    RemFAT(Search(Name));
+  case 'M':
+    Infile >> Name >> Size;
+    EditFAT(Search(Name), Size);
+  case 'R':
+    Infile >> Name >> NewName;
+    Name(Name, NewName);
+  case '?':
+    return 0;
+  }
+  time++;
 }
